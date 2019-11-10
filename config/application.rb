3e675daddcd8da6e38a00133534436130454e65e
Meg Gutshall <meghan.gutshall@gmail.com>
2019-11-10 17:19:00 -0500

    Edit various app configs

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,7 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# Require the gems listed in Gemfile, including any gems you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module CodeCars
@@ -24,10 +23,7 @@ module CodeCars
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Settings in config/environments/* take precedence over those specified here. Application configuration can go into files in config/initializers -- all .rb files in that directory are automatically loaded after loading the framework and any gems in your application.
 
     # Don't generate system test files.
     config.generators.system_tests = nil
