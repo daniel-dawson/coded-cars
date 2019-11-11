5917a8419cfc9554e4fc5533c6ba719320487ec9
Meg Gutshall <meghan.gutshall@gmail.com>
2019-11-10 20:38:09 -0500

    Organize routes and admin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Root route (Home)
+  root to: 'welcome#home'
+
+  # Devise routes
+  devise_for :admins
+
   resources :ownership_histories
   resources :cars
   resources :owners
-  devise_for :admins
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
