448087823113ab327663d87a505481d091480731
Meg Gutshall <meghan.gutshall@gmail.com>
2019-11-11 00:26:37 -0500

    Seed database using faker gem

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ group :development, :test do
   gem 'devise'
   # Use RSpec for back-end testing
   gem 'rspec-rails', '~> 3.8'
+  # Creates dummy data for test instances
+  gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry', '~> 0.12.2'
@@ -59,11 +61,11 @@ group :test do
   gem 'webdrivers'
   # Creates test objects
   gem 'factory_bot_rails', '~> 5.0'
-  # Creates dummy data for test instances
-  gem 'faker', '~> 1.9'
   # Provides one-line test code
   gem 'shoulda-matchers', '~> 4.1'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "rubocop-faker", "~> 0.2.0"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+require 'faker'
+
+# This file should contain all the record creation needed to seed the database with its default values. The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Faker configurations
+Faker::Config.random = Random.new(42)
+Faker::Config.locale = 'en-US'
+
+# Admin object
+@admin = Admin.create!(
+  email: "coded-cars@thrivecommerce.com",
+  password: "password"
+)
+
+# Owner objects
+def owner_data
+  name = Faker::FunnyName.two_word_name
+  email = name.gsub(' ', '.').downcase! + "@gmail.com"
+  phone = Faker::PhoneNumber.cell_phone
+  admin = @admin
+  {admin: admin, name: name, email: email, telephone: phone}
+end
+
+Faker::Number.between(from: 8, to: 14).times do
+  Owner.create!(owner_data)
+end
+
+# Car objects
+def car_data
+  make = Faker::Vehicle.make
+  model = Faker::Vehicle.model(make_of_model: make)
+  color = Faker::Vehicle.color
+  mileage = Faker::Vehicle.mileage
+  is_for_sale = Faker::Boolean.boolean(true_ratio: 0.3)
+  admin = @admin
+  {admin: admin, make: make, model: model, color: color, mileage: mileage, is_for_sale: is_for_sale}
+end
+
+Owner.all.count.times do
+  Car.create!(car_data)
+end
+
+# Ownership history objects
+Owner.all.count.times do
+  OwnershipHistory.create!(
+    owner: Owner.all.sample,
+    car: Car.all.sample,
+    admin: @admin
+  )
+end
