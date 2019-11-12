cca6894d69d998ff2a69e3325e4a17a99f9186d8
Meg Gutshall <meghan.gutshall@gmail.com>
2019-11-12 17:37:13 -0500

    Add Car#current_owner method

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,16 +2,8 @@ class Car < ApplicationRecord
   has_many :ownership_histories, dependent: :delete_all
   has_many :owners, through: :ownership_histories
 
-
-  # Owner setter and getter
-  # Create database call for most recent Car owner
-  def owner=(owner)
-    @owner = Owner.all.sample
+  def current_owner
+    self.owners.last
   end
-
-  def owner
-    @owner
-  end
-
 
 end
